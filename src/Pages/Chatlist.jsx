c3628c0dfc22e84ba9a60867a09ba8ea5c0677cb
RajeshK-pds <RajeshKollipara@Privacydatasystems.com>
2023-09-02 17:37:53 +0530

    badge icon

--- src/Pages/Chatlist.jsx
+++ src/Pages/Chatlist.jsx
@@ -1,6 +1,11 @@
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  Badge,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import React, { useEffect } from "react";
-
 const ChatList = ({ chat }) => {
   return (
     <ListItem>
@@ -8,6 +13,7 @@ const ChatList = ({ chat }) => {
         <Avatar src={chat.profilepicture} alt={chat.name} />
       </ListItemAvatar>
       <ListItemText primary={chat.name} secondary={chat.message} />
+      <Badge color="success" variant="dot"></Badge>
     </ListItem>
   );
 };
